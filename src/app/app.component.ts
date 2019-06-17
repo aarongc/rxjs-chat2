4f471729668ad599b0580eaa153785ed0e1a42b4
aaron castanos <ron.gcastanos@gmail.com>
2019-06-17 17:34:20 +0800

    .gitignore fix

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { ChatService } from '../service/chat.service';
 
 @Component({
   selector: 'app-root',
@@ -7,4 +8,21 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'rxjs-chat';
+  message: string;
+  messages: string[] = [];
+  
+  constructor(private chatService: ChatService) {}
+
+  sendMessage() {
+    this.chatService.sendMessage(this.message);
+    this.message = '';
+  }
+
+  ngOnInit() {
+    this.chatService
+      .getMessages()
+      .subscribe((message: string) => {
+        this.messages.push(message);
+      });
+  }
 }
